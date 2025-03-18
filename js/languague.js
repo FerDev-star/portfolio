ed6f92eae2058d439dd1822bed95684dbabe29dc
Fernando Mora <fer.developer.m@gmail.com>
2025-03-17 20:53:30 -0600

    Add files via upload

---
+++ js/languague.js
@@ -0,0 +1,194 @@
+const translations = {
+    en: {
+        menuItem1: "Home",
+        menuItem2: "About Me",
+        menuItem3: "Services",
+        menuItem4: "Portfolio",
+        menuItem5: "Contact",
+        copyright: "© 2024 | Designed & Developed by Fernando Mora",
+        aboutTitle: "I'm Fernando Mora, <span>Web Developer</span>",
+        aboutText: "Web developer with experience in modern technologies like Wordpress, Laravel, MySQL, Git, and Next.js. I have worked on interactive, user-optimized projects, including building responsive websites, customizing WooCommerce stores, and implementing innovative features. Additionally, I have competed in international events such as WorldSkills Costa Rica and WorldSkills Lyon 2024, where I showcased my technical skills under pressure. My focus is always on improving user experience through web development.",
+        birthday: "Birthday : <span>2 July 2002</span>",
+        age: "Age : <span>22 years old</span>",
+        website: "Website : <span>ferdev-star.com</span>",
+        email: "Email : <span>fer.developer.m@gmail.com</span>",
+        degree: "Degree : <span>Web developer</span>",
+        phone: "Phone : <span>+506 63769174</span>",
+        city: "City : <span>San Jose</span>",
+        freelance: "Freelance : <span>Available</span>",
+        languages: "Languages : <span class='lang-span'>Spanish | English</span>",
+        downloadCv: "Resume",
+        hireMe: "Hire me",
+        educationTitle: "Education",
+        uxTitle: "UX UI design",
+        uxText: "Completed a UX/UI design program at Instituto Nacional de Aprendizaje (INA), where I gained knowledge in HTML, CSS, and JavaScript. Additionally, I learned how to create prototypes using Adobe XD and worked with WordPress for web design and content management, and explored the integration of APIs to enhance user interactions and dynamic content.",
+        kstarTitle: "2024 K-STAR Capacity Building Program",
+        kstarText: "Participated in this international program that enhanced my skills in web development, problem-solving, and teamwork in a technology-driven environment. During the program, I learned Laravel, React.js, and MySQL databases, which strengthened my backend and frontend development capabilities.",
+        experienceTitle: "Experience",
+        wpDeveloperTitle: "Wordpress developer",
+        wpDeveloperText: "Redesigned and developed the <strong>Sierpe Mangrove Tour</strong> website by creating a custom WordPress theme. Improved the site's responsiveness, optimized the design, and configured WooCommerce for seamless tour bookings and online transactions.",
+        frontendDeveloperTitle: "Frontend Developer",
+        frontendDeveloperText: "Developed the <strong>Ignite</strong> website using <strong>Next.js</strong> and <strong>Sass</strong>, focusing on performance, modern UI/UX principles, and a fully responsive layout. Implemented dynamic product filtering with React state management, ensuring an interactive and seamless user experience. Designed reusable components for product showcases, optimizing maintainability and scalability.",
+        servicesTitle: "Services",
+        webDesignTitle: "Web Design & UX/UI",
+        webDesignText: "Creating modern, user-friendly, and visually appealing websites with a focus on UX/UI principles. Skilled in Figma, wireframing, prototyping, and responsive design.",
+        webDevTitle: "Web Development",
+        webDevText: "Building scalable and high-performance websites using HTML, CSS, JavaScript, Laravel, Vue.js, Next.js, and WordPress. Experience in API integration and dynamic content handling.",
+        seoOptimizationTitle: "SEO & Performance Optimization",
+        seoOptimizationText: "Optimizing websites for better search engine rankings and faster load times. Implementing SEO best practices, image compression, and performance improvements.",
+        portfolioTitle: "Portfolio",
+        responsiveWebDesignTitle: "Responsive Web Design",
+        responsiveWebDesignDescription: "Redesigned and developed the <strong>Sierpe Mangrove Tour</strong> website by creating a custom WordPress theme. Improved the site's responsiveness, optimized the design, and configured WooCommerce for seamless tour bookings and online transactions.",
+        igniteWebsiteTitle: "Ignite website",
+        igniteWebsiteDescription: "Developed the <strong>Ignite</strong> website using <strong>Next.js</strong> and <strong>Sass</strong>, focusing on performance, modern UI/UX principles, and a fully responsive layout. Implemented dynamic product filtering with React state management, ensuring an interactive and seamless user experience. Designed reusable components for product showcases, optimizing maintainability and scalability.",
+        tags1: "Tags : <a>WordPress</a> , <a>Php</a>",
+        tags2: "Etiquetas : <a>Next.js</a> , <a>Sass</a>",
+        contactTitle: "Contact Me",
+        callMeOn: "Call me on",
+        phoneNumber: "+506 63769174",
+        office: "Office",
+        officeLocation: "Costa Rica",
+        email1: "Email",
+        emailAddress: "fer.developer.m@gmail.com",
+        typedStrings: [
+            "< I'm a Web Developer />", 
+            "< I Love Html5 />", 
+            "< I Love CSS3 />", 
+            "< I Love React />", 
+            "< I Love Laravel />", 
+            "< I Love Wordpress />"
+        ],
+    },
+    es: {
+        menuItem1: "Inicio",
+        menuItem2: "Sobre Mí",
+        menuItem3: "Servicios",
+        menuItem4: "Portafolio",
+        menuItem5: "Contacto",
+        copyright: "© 2024 | Diseñado y desarrollado por Fernando Mora",
+        aboutTitle: "Soy Fernando Mora, <span>Desarrollador Web</span>",
+        aboutText: "Desarrollador web con experiencia en tecnologías modernas como Wordpress, Laravel, MySQL, Git y Next.js. He trabajado en proyectos interactivos optimizados para el usuario, incluyendo la creación de sitios web responsivos, personalización de tiendas WooCommerce e implementación de características innovadoras. Además, he competido en eventos internacionales como WorldSkills Costa Rica y WorldSkills Lyon 2024, donde mostré mis habilidades técnicas bajo presión. Mi enfoque siempre está en mejorar la experiencia del usuario a través del desarrollo web.",
+        birthday: "Cumpleaños : <span>2 de julio de 2002</span>",
+        age: "Edad : <span>22 años</span>",
+        website: "Sitio web : <span>ferdev-star.com</span>",
+        email: "Correo : <span>fer.developer.m@gmail.com</span>",
+        degree: "Título : <span>Desarrollador web</span>",
+        phone: "Teléfono : <span>+506 63769174</span>",
+        city: "Ciudad : <span>San José</span>",
+        freelance: "Freelance : <span>Disponible</span>",
+        languages: "Idiomas : <span class='lang-span'>Español | Inglés</span>",
+        downloadCv: "Descargar CV",
+        hireMe: "Contratarme",
+        educationTitle: "Educación",
+        uxTitle: "Diseño UX/UI",
+        uxText: "Completé un programa de diseño UX/UI en el Instituto Nacional de Aprendizaje (INA), donde adquirí conocimientos en HTML, CSS y JavaScript. Además, aprendí a crear prototipos utilizando Adobe XD y trabajé con WordPress para diseño web y gestión de contenido, además de explorar la integración de APIs para mejorar las interacciones con el usuario y el contenido dinámico.",
+        kstarTitle: "Programa de Capacitación K-STAR 2024",
+        kstarText: "Participé en este programa internacional que mejoró mis habilidades en desarrollo web, resolución de problemas y trabajo en equipo en un entorno impulsado por la tecnología. Durante el programa, aprendí Laravel, React.js y bases de datos MySQL, lo que fortaleció mis capacidades de desarrollo tanto en backend como en frontend.",
+        experienceTitle: "Experiencia",
+        wpDeveloperTitle: "Desarrollador WordPress",
+        wpDeveloperText: "Rediseñé y desarrollé el sitio web <strong>Sierpe Mangrove Tour</strong> creando un tema personalizado en WordPress. Mejoré la capacidad de respuesta del sitio, optimicé el diseño y configuré WooCommerce para realizar reservas de tours y transacciones en línea sin problemas.",
+        frontendDeveloperTitle: "Desarrollador Frontend",
+        frontendDeveloperText: "Desarrollé el sitio web <strong>Ignite</strong> utilizando <strong>Next.js</strong> y <strong>Sass</strong>, enfocándome en el rendimiento, principios modernos de UI/UX y un diseño completamente responsivo. Implementé filtrado dinámico de productos con gestión de estado en React, asegurando una experiencia de usuario interactiva y fluida. Diseñé componentes reutilizables para la exhibición de productos, optimizando la mantenibilidad y escalabilidad.",
+        servicesTitle: "Servicios",
+        webDesignTitle: "Diseño Web & UX/UI",
+        webDesignText: "Creando sitios web modernos, fáciles de usar y visualmente atractivos con un enfoque en los principios de UX/UI. Habilidad en Figma, wireframing, prototipos y diseño responsivo.",
+        webDevTitle: "Desarrollo Web",
+        webDevText: "Construcción de sitios web escalables y de alto rendimiento utilizando HTML, CSS, JavaScript, Laravel, Vue.js, Next.js y WordPress. Experiencia en integración de APIs y manejo de contenido dinámico.",
+        seoOptimizationTitle: "SEO & Optimización de Rendimiento",
+        seoOptimizationText: "Optimizando sitios web para obtener mejores posiciones en los motores de búsqueda y tiempos de carga más rápidos. Implementación de mejores prácticas de SEO, compresión de imágenes y mejoras de rendimiento.",
+        portfolioTitle: "Portafolio",
+        responsiveWebDesignTitle: "Diseño Web Responsivo",
+        responsiveWebDesignDescription: "Rediseñé y desarrollé el sitio web <strong>Sierpe Mangrove Tour</strong> creando un tema personalizado en WordPress. Mejoré la capacidad de respuesta del sitio, optimicé el diseño y configuré WooCommerce para una reserva de tours y transacciones en línea sin problemas.",
+        igniteWebsiteTitle: "Sitio web Ignite",
+        igniteWebsiteDescription: "Desarrollé el sitio web <strong>Ignite</strong> utilizando <strong>Next.js</strong> y <strong>Sass</strong>, enfocándome en el rendimiento, principios modernos de UI/UX y un diseño completamente responsivo. Implementé filtrado dinámico de productos con gestión de estado en React, asegurando una experiencia de usuario interactiva y fluida. Diseñé componentes reutilizables para mostrar productos, optimizando el mantenimiento y la escalabilidad.",
+        tags1: "Tags : <a>WordPress</a> , <a>Php</a>",
+        tags2: "Etiquetas : <a>Next.js</a> , <a>Sass</a>",
+        contactTitle: "Contáctame",
+        callMeOn: "Llámame",
+        phoneNumber: "+506 63769174",
+        office: "Oficina",
+        officeLocation: "Costa Rica",
+        email1: "Correo",
+        emailAddress: "fer.developer.m@gmail.com",
+        typedStrings: [
+            "< Soy un Desarrollador Web />", 
+            "< Me encanta Html5 />", 
+            "< Me encanta CSS3 />", 
+            "< Me encanta React />", 
+            "< Me encanta Laravel />", 
+            "< Me encanta Wordpress />"
+        ],
+    }
+};
+
+const link = document.getElementById("resumeLink");
+
+function changeLanguage(lang) {
+    // Cambiar el idioma en los elementos de la página
+    document.querySelectorAll("[data-translate]").forEach(element => {
+        const key = element.getAttribute("data-translate");
+        if (translations[lang] && translations[lang][key]) {
+            // Mantener el icono y solo cambiar el texto después de él
+            const icon = element.querySelector("i"); // Encuentra el ícono
+            const translatedText = translations[lang][key]; // Obtiene el texto traducido
+
+            // Limpia el contenido original del elemento
+            element.innerHTML = ""; 
+
+            // Vuelve a agregar el icono si existe
+            if (icon) {
+                element.appendChild(icon);
+            }
+
+            // Usamos innerHTML para insertar el texto traducido con etiquetas HTML
+            element.innerHTML += translatedText;  // Agrega el contenido traducido con HTML
+        }
+    });
+
+    if (lang === "en") {
+        link.href = "./resume/Fernando-Mora-EN.pdf"
+    } else {
+        link.href = "./resume/Fernando-Mora-ES.pdf"
+    }
+
+    const typedElementContainer = document.querySelector('.container_itype');
+    
+    // Eliminar el 'span' con la clase 'iTyped' si existe
+    const typedElement = typedElementContainer.querySelector('.iTyped');
+    // const cursorElement = typedElementContainer.querySelector('.ityped-cursor');
+    if (typedElement) {
+        typedElement.remove(); 
+        // cursorElement.remove();
+    }
+
+    // Crear un nuevo 'span' y agregarlo al contenedor
+    const newTypedElement = document.createElement('span');
+    newTypedElement.classList.add('iTyped');
+    typedElementContainer.appendChild(newTypedElement);
+
+
+    // Inicializar ityped con las nuevas cadenas de texto según el idioma seleccionado
+    setTimeout(() => {
+        window.ityped.init(newTypedElement, {
+            strings: translations[lang].typedStrings,
+            loop: true,
+            typeSpeed: 100, // Puedes ajustar la velocidad de tipeo si es necesario
+            backSpeed: 50,  // Puedes ajustar la velocidad de borrado si es necesario
+            cursor: "|",  // Especifica el cursor si lo necesitas
+        });
+    }, 50);
+
+}
+
+// Cargar idioma por defecto
+document.addEventListener("DOMContentLoaded", () => {
+    changeLanguage("en");  // O el idioma predeterminado
+});
+
+
+// Escuchar cambios en los radio buttons para cambiar el idioma
+document.querySelectorAll(".language-switcher").forEach(input => {
+    input.addEventListener("change", function () {
+        changeLanguage(this.value);  // Cambiar idioma cuando el usuario selecciona un radio button
+    });
+});
